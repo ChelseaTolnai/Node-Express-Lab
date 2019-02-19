a43499ab3d472a377cab0ee41786281542bd7dc8
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-18 22:05:46 -0500

    Refractor post request to be async. Created get post request with find method.

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -3,21 +3,29 @@ const db = require('../db')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     if(!req.body.title || ! req.body.contents) {
         res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
         return;
     } else {
-        db
-        .insert(req.body)
-        .then(post => {
+        try {
+            const post = await db.insert(req.body);
             res.status(201).json(post);
-        })
-        .catch (error => {
+        } catch (error) {
             console.log(error);
-            res.status(500).json({ error: 'There was an error while saving the post to the database' });
-        });
+            res.status(500).json({ error: 'The posts information could not be retrieved.' });
+        }
     }
-})
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await db.find(req.query);
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    }
+  });
 
 module.exports = router;
