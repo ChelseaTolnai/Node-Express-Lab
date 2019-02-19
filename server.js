87120b4e1a95275c73573a2b95f02ec508c91bba
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-18 20:56:33 -0500

    Setup of posts router for api/posts endpoints.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const postsRouter = require('./data/posts/posts-router')
 const server = express();
 
 server.use(express.json());
+server.use('/api/posts', postsRouter);
 
 module.exports = server;
